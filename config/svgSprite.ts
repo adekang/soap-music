d219ea180150099d1d39e255989aa47e60cf240b
adekang <adekang@163.com>
2023-01-13 21:57:11 +0800

    chore: svg fill

--- config/svgSprite.ts
+++ config/svgSprite.ts
@@ -8,7 +8,14 @@ export default function configSvgSprites() {
       // 指定需要缓存的图标文件夹
       iconDirs: [path.resolve(process.cwd(), "./src/assets/icons")],
       // 指定symbolId格式
-      symbolId: "icon-[dir]-[name]"
+      symbolId: "icon-[dir]-[name]",
+      svgoOptions: {
+        plugins: [{
+          name: "removeAttrs",
+          params: { attrs: ["class", "data-name", "fill", "stroke"] }
+        }]
+      }
+
 
       /**
        * 自定义插入位置
