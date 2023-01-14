83814e95d36d381603fe893e9670c6ac30e1588d
adekang <adekang@163.com>
2023-01-14 12:17:43 +0800

    feat: index推荐

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,9 +1,143 @@
 import React from "react";
+import "./index.scss";
 
 const Index = () => {
   return (
     <div className="index">
-      <h1>Index</h1>
+      <div className="title">
+        <h1>推荐歌单</h1>
+        <span>
+          <a href="#">查看全部</a>
+        </span>
+      </div>
+      <div className="box-container">
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">
+              这节奏，听着就想跳舞！这节奏，听着就想跳舞！这节奏，听着就想跳舞！这节奏，听着就想跳舞！
+            </div>
+            <div className="info">
+              by music 这节奏，听着就想跳舞！这节奏，听着就想跳舞！这节奏，听着就想跳舞！
+            </div>
+          </div>
+        </div>
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">这节奏，听着就想</div>
+            <div className="info">by music</div>
+          </div>
+        </div>
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">这节奏，听着就想跳舞！</div>
+            <div className="info">by music</div>
+          </div>
+        </div>
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">这节奏，听着就想跳舞！</div>
+            <div className="info">by music</div>
+          </div>
+        </div>
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">这节奏，听着就想跳舞！</div>
+            <div className="info">by music</div>
+          </div>
+        </div>{" "}
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">
+              这节奏，听着就想跳舞！这节奏，听着就想跳舞！这节奏，听着就想跳舞！这节奏，听着就想跳舞！
+            </div>
+            <div className="info">by music</div>
+          </div>
+        </div>
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">这节奏，听着就想跳舞！</div>
+            <div className="info">by music</div>
+          </div>
+        </div>
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">这节奏，听着就想跳舞！</div>
+            <div className="info">by music</div>
+          </div>
+        </div>
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">这节奏，听着就想跳舞！</div>
+            <div className="info">by music</div>
+          </div>
+        </div>
+        <div className="box-item">
+          <div className="cover">
+            <img
+              src="https://p2.music.126.net/GvYQoflE99eoeGi9jG4Bsw==/109951165375336156.jpg?param=512y512"
+              loading="lazy"
+            />
+          </div>
+          <div className="text">
+            <div className="title">这节奏，听着就想跳舞！</div>
+            <div className="info">by music</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
